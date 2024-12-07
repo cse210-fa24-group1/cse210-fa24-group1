8f013d60f23fbf700c48eec0522eb45fa9200e55
Ketaki Tank <ketakitank@Ketakis-Air.lan1>
2024-12-07 14:10:50 -0800

    fixed linting issues

--- src/script.js
+++ src/script.js
@@ -8,61 +8,60 @@ const form = document.getElementById('form');
 const textInput = document.querySelector('#text');
 const amount = document.querySelector('#amount');
 
-
 // const dummyTransactions = [
 //     {id : 1 , text : 'Flower' , amount : -20},
 //     {id : 2 , text : 'Salary' , amount :  300},
 //     {id : 3 , text : 'Book' , amount :  -10},
-//     {id : 4 , text : 'Camera' , amount :  150},  
+//     {id : 4 , text : 'Camera' , amount :  150},
 // ];
 
-const localStorageTransactions = JSON.parse(localStorage.getItem('transactions')); //it will string so apply JSON parse to make it array
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+); //it will string so apply JSON parse to make it array
 
-let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];//if local storage is none set localStorageTransactions to empty array
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : []; //if local storage is none set localStorageTransactions to empty array
 
 //Add Transaction
-function addTransaction(e){
-    if(textInput.value.trim() === '' || amount.value.trim() === '') {
-        alert('Enter Add Text and Amount First');
-    }
-    else{
-        const transaction = {
-            id: generateID(),
-            text: text.value,
-            amount: +amount.value
-        };
+function addTransaction(e) {
+  if (textInput.value.trim() === '' || amount.value.trim() === '') {
+    alert('Enter Add Text and Amount First');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
 
-        transactions.push(transaction);
+    transactions.push(transaction);
 
-        addTransactionDOM(transaction);
+    addTransactionDOM(transaction);
 
-        updateValues();
+    updateValues();
 
-        updateLocalStorage();
+    updateLocalStorage();
 
-        text.value = '';
-        amount.value = '';
-    }
+    text.value = '';
+    amount.value = '';
+  }
 
-    e.preventDefault();
+  e.preventDefault();
 }
 
 // Generate random ID
 function generateID() {
-    return Math.floor(Math.random() * 100000000);
+  return Math.floor(Math.random() * 100000000);
 }
-
 
 //Add transactions to DOM list
 function addTransactionDOM(transaction) {
-    //get sign
-    const sign = transaction.amount < 0 ? '-' : '+';
-    const item = document.createElement('li');
-    
-    //Add class based on value
-    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-    item.innerHTML = 
-    `
+  //get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+
+  //Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.innerHTML = `
     ${transaction.text}
     <span>${transaction.amount}</span>
     <button class="delete-btn" 
@@ -70,70 +69,70 @@ function addTransactionDOM(transaction) {
     x</button>
     `;
 
-//<span>${sign}${Math.abs(transaction.amount)}<.span>
+  //<span>${sign}${Math.abs(transaction.amount)}<.span>
 
-    list.appendChild(item);
+  list.appendChild(item);
 }
 
 //Remove Transaction by Id
-function removeTransaction(id){
-    transactions = transactions.filter(transaction =>
-    transaction.id !== id);
-    
-    updateLocalStorage();
-    init();
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  updateLocalStorage();
+  init();
 }
 
-
 //Update the balance ,income and expense
-function updateValues(){
-    
-    const amounts = transactions.map(transaction => transaction.amount);
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
 
-    const total = amounts.reduce((acc,item) => (acc += item),0).toFixed(2);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
-    const income = amounts.filter(item => item > 0).reduce((acc,item) => (acc += item),0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
 
-    const expense = (amounts.
-                        filter(item => item < 0).
-                        reduce((acc,item) => (acc += item),0) * -1).
-                        toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
 
-    balance.innerText = `$${total}`;
-    money_plus.innerText = `$${income}`;
-    money_minus.innerText = `$${expense}`;
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
 }
 
 //Update Local Storage Transaction
-function updateLocalStorage(){
-    localStorage.setItem('transactions',JSON.stringify(transactions));
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
 //Init app
-function init(){
-    list.innerHTML = '';
-    transactions.forEach(addTransactionDOM);
-    updateValues();
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
 
-form.addEventListener('submit',addTransaction);
+form.addEventListener('submit', addTransaction);
 
 //Screenshot and downloading
-const capture = document.querySelector("#capture");
-var button = document.getElementById("btn-download");
-const box = document.querySelector(".box");
-box.style.display = "none";
-button.style.display = "none";
+const capture = document.querySelector('#capture');
+var button = document.getElementById('btn-download');
+const box = document.querySelector('.box');
+box.style.display = 'none';
+button.style.display = 'none';
 
-capture.addEventListener("click", function () {
+capture.addEventListener('click', function () {
   html2canvas(document.body).then((canvas) => {
     text = `Screenshot Captured`;
-    box.style.display = "block"; 
-    button.style.display = "block"; 
+    box.style.display = 'block';
+    button.style.display = 'block';
     box.innerHTML = text;
-    var dataURL = canvas.toDataURL("image/png");
+    var dataURL = canvas.toDataURL('image/png');
     button.href = dataURL;
     button.download = `ScreenShot.png`; // download name
   });
